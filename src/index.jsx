cf1cd034418dcd20cbe93cbf9d8c2c75f2d390f2
0 <malakbalaiman@gmail.com>
2024-03-10 19:47:10 +0000

    install pkg

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
