c51d5824c692f6f2e723ac17332c1d256b86e9f1
0 <malakbalaiman@gmail.com>
2024-03-11 17:27:04 +0000

    add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,45 @@
-import {Message} from "primereact/message"
-import {DataTable} from "primereact/datatable"
-import {Column} from "primereact/column"
+import { Message } from "primereact/message";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Menubar } from "primereact/menubar";
 
-const d=[{
-  name:"rae lil black"
-},{
-  name:"rae lil black"
-},{
-  name:"rae lil black"
-},{
-  name:"rae lil black"
-},{
-  name:"rae lil black"
-},{
-  name:"rae lil black"
-}]
-
+const items = [
+  {
+    label: "Update",
+  },
+  {
+    label: "Delete",
+  },
+];
+const d = [
+  {
+    name: "rae lil black",
+  },
+  {
+    name: "rae lil black",
+  },
+  {
+    name: "rae lil black",
+  },
+  {
+    name: "rae lil black",
+  },
+  {
+    name: "rae lil black",
+  },
+  {
+    name: "rae lil black",
+  },
+];
 
 function App() {
   return (
     <>
-      <Message text="امروز تایمتون خالیه"/>
-<DataTable value={d}>
-  <Column field="name"/>
-</DataTable>
+      <Menubar model={items} />
+      <Message text="امروز تایمتون خالیه" />
+      <DataTable value={d}>
+        <Column field="name" />
+      </DataTable>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import "./theme.css"
 
 const router = createBrowserRouter([
